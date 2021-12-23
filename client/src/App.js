371a8d4d5d37b065de5b9072730d251fb34e8d6a
HacAtac <jhack00@icloud.com>
2021-12-23 08:53:58 -0600

    added react-router to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+
 import {
   ApolloProvider, // ApolloProvider is a react component that provides the client to the entire application will be a hook
   ApolloClient, // ApoloClient is the client that connects to the server
@@ -24,13 +31,23 @@ function App() {
   //here we are passing in the client to the ApolloProvider component so we can use the client in the entire application
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1>Deep Thoughts</h1>
+        <Link to="/" className="text-white">
+          <h1 className="text-white">HacAtac MERNql</h1>
+        </Link>
+
+        <nav className="text-center text-white">
+          <Link to="/login" className="text-white">
+            {" "}
+            Login{" "}
+          </Link>
+          <Link to="/signup" className="text-white">
+            {" "}
+            Signup{" "}
+          </Link>
+        </nav>
       </div>
     </header>
   );
